5f245841f2b10d432912d4793c42825eb06b3ee9
GIFTED DAVE <comradeanasafa@gmail.com>
2025-06-28 18:06:39 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,117 +1,112 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
 const QRCode = require('qrcode');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-let router = express.Router()
+let router = express.Router();
 const pino = require("pino");
 const {
-        default: Mbuvi_Tech,
-        useMultiFileAuthState,
-        jidNormalizedUser,
-        Browsers,
-        delay,
-        makeInMemoryStore,
+    default: makeConnection,
+    useMultiFileAuthState,
+    jidNormalizedUser,
+    Browsers,
+    delay,
+    makeInMemoryStore,
 } = require("@whiskeysockets/baileys");
 
 function removeFile(FilePath) {
-        if (!fs.existsSync(FilePath)) return false;
-        fs.rmSync(FilePath, {
-                recursive: true,
-                force: true
-        })
-};
-const {
-        readFile
-} = require("node:fs/promises")
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, {
+        recursive: true,
+        force: true
+    });
+}
+
 router.get('/', async (req, res) => {
-        const id = makeid();
-        async function MBUVI_MD_QR_CODE() {
-                const {
-                        state,
-                        saveCreds
-                } = await useMultiFileAuthState('./temp/' + id)
-                try {
-                        let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
-                                auth: state,
-                                printQRInTerminal: false,
-                                logger: pino({
-                                        level: "silent"
-                                }),
-                                browser: Browsers.macOS("Desktop"),
-                        });
+    const id = makeid();
 
-                        Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-                        Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-                                const {
-                                        connection,
-                                        lastDisconnect,
-                                        qr
-                                } = s;
-                                if (qr) await res.end(await QRCode.toBuffer(qr));
-                                if (connection == "open") {
-                                        await delay(5000);
-                                        let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                                        await delay(800);
-                                   let b64data = Buffer.from(data).toString('base64');
-                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Bellah~' + b64data });
+    async function generateQRCodeSession() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let connection = makeConnection({
+                auth: state,
+                printQRInTerminal: false,
+                logger: pino({ level: "silent" }),
+                browser: Browsers.macOS("Desktop"),
+            });
 
-                                   let MBUVI_MD_TEXT = `
+            connection.ev.on('creds.update', saveCreds);
+
+            connection.ev.on("connection.update", async (s) => {
+                const { connection: conn, lastDisconnect, qr } = s;
+
+                if (qr) await res.end(await QRCode.toBuffer(qr));
+
+                if (conn === "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(800);
+                    let b64data = Buffer.from(data).toString('base64');
+                    let session = await connection.sendMessage(connection.user.id, { text: 'DaveMd~' + b64data });
+
+                    let sessionText = `
+
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨ DAVE-XMD🔷
+║ ✨ DAVE SESSIONS 🔷
 ║ ✨ Gifted Dave 🔷
 ╚════════════════════╝
 
-
 ---
 
 ╔════════════════════◇
-║『 YOU'VE CHOSEN DAVE-XMD 』
-║ -Set the session ID in Heroku:
-║ - SESSION_ID: 
+║『 YOU'VE CHOSEN DAVE SESSIONS 』
+║ - Set the session ID in your bot environment:
+║ - SESSION_ID: DaveMd~...
 ╚════════════════════╝
-╔════════════════════◇
-║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@davlodavlo19
-║❍ 𝐎𝐰𝐧𝐞𝐫: 254104260236
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/gifteddaves/DAVE-XMD
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/CaPeB0sVRTrL3aG6asYeAC
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k
-║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/giddy_tennor_?igsh=YzZ0NDRoaXFxM2Zk
-║ ☬ ☬ ☬ ☬
-╚═════════════════════╝
-𒂀 Enjoy DAVE-XMD
 
+╔════════════════════◇
+║ 『••• VISIT FOR HELP •••』
+║❍ YouTube: youtube.com/@davlodavlo19
+║❍ Owner: 254104260236
+║❍ Repo: https://github.com/gifteddaves/DAVE-Md-V1
+║❍ WaGroup: https://chat.whatsapp.com/CaPeB0sVRTrL3aG6asYeAC
+║❍ WaChannel: https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k
+║❍ Instagram: https://www.instagram.com/_gifted_dave?igsh=YzZ0NDRoaXFxM2Zk
+╚═════════════════════╝
+𒂀 Enjoy DaveMd Sessions!
 
 ---
 
-Don't Forget To Give Star⭐ To My Repo
-______________________________`;
-         await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
+Don't Forget To ⭐ Star The Repo!
+____________________________________`;
 
-
-
-                                        await delay(100);
-                                        await Qr_Code_By_Mbuvi_Tech.ws.close();
-                                        return await removeFile("temp/" + id);
-                                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                                        await delay(10000);
-                                        MBUVI_MD_QR_CODE();
-                                }
-                        });
-                } catch (err) {
-                        if (!res.headersSent) {
-                                await res.json({
-                                        code: "Service is Currently Unavailable"
-                                });
-                        }
-                        console.log(err);
-                        await removeFile("temp/" + id);
+                    await connection.sendMessage(connection.user.id, { text: sessionText }, { quoted: session });
+                    await delay(100);
+                    await connection.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (
+                    conn === "close" &&
+                    lastDisconnect &&
+                    lastDisconnect.error &&
+                    lastDisconnect.error.output.statusCode != 401
+                ) {
+                    await delay(10000);
+                    generateQRCodeSession();
                 }
+            });
+        } catch (err) {
+            if (!res.headersSent) {
+                await res.json({ code: "Service is Currently Unavailable" });
+            }
+            console.log(err);
+            await removeFile('./temp/' + id);
         }
-        return await MBUVI_MD_QR_CODE()
+    }
+
+    return await generateQRCodeSession();
 });
-module.exports = router
+
+module.exports = router;
